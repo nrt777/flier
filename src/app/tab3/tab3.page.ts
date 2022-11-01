3207f7785497b14826f24c54b4dd8ce8a74b29e8
Mikku44 <104062911+Mikku44@users.noreply.github.com>
2022-11-02 00:22:41 +0700

    Summary

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
-
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  styleUrls: ['tab3.page.scss'],
 })
 export class Tab3Page {
+  handlerMessage = '';
+  roleMessage = '';
+  constructor(private alertController: AlertController) {}
 
-  constructor() {}
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Warning',
+      subHeader: '',
+      message: 'Do you want to receive file from {other Device}?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'alert-button-cancel',
+          handler: () => {
+            this.handlerMessage = 'Alert cancaled';
+          },
+        },
+        {
+          text: 'Accept',
+          role: 'confirm',
+          handler: () => {
+            this.handlerMessage = 'Alert confirmed';
+          },
+        },
+      ],
+    });
 
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    this.roleMessage = `Dismissed with role: ${role}`;
+  }
 }
