00fe0cdc5d5eceb86e01493d438b72453a0e4303
Mikku44 <104062911+Mikku44@users.noreply.github.com>
2022-11-02 12:54:44 +0700

    add plugin

--- src/app/download/download.page.ts
+++ src/app/download/download.page.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { Router } from '@angular/router';
+
+import {
+  FileTransfer,
+  FileUploadOptions,
+  FileTransferObject,
+} from '@awesome-cordova-plugins/file-transfer/ngx';
+// import { File } from '@awesome-cordova-plugins/file';
+
 @Component({
   selector: 'app-download',
   templateUrl: './download.page.html',
@@ -9,9 +17,13 @@ import { Router } from '@angular/router';
 export class DownloadPage implements OnInit {
   handlerMessage = '';
   roleMessage = '';
+  // fileTransfer: FileTransferObject = this.transfer.create();
+  dataDirectory: string;
   constructor(
     private alertController: AlertController,
-    private router: Router
+    private router: Router,
+    // private transfer: FileTransfer,
+    // private file: File
   ) {}
 
   async presentAlert() {
@@ -32,7 +44,7 @@ export class DownloadPage implements OnInit {
           text: 'Accept',
           role: 'confirm',
           handler: () => {
-            this.router.navigate(['/download']);
+            this.router.navigate(['/tabs']);
           },
         },
       ],
@@ -42,6 +54,23 @@ export class DownloadPage implements OnInit {
 
     const { role } = await alert.onDidDismiss();
     this.roleMessage = `Dismissed with role: ${role}`;
+
+    // Download a file:
+    function downloadFunc() {
+      const url = 'http://www.example.com/file.pdf';
+      this.fileTransfer.download(url, this.dataDirectory + 'file.pdf').then(
+        (entry) => {
+          console.log('download complete: ' + entry.toURL());
+        },
+        (error) => {
+          // handle error
+        }
+      );
+    }
+    //view more at https://ionicframework.com/docs/native/file-transfer
+
+    // Abort active transfer: cancel
+    // this.fileTransfer.abort();
   }
 
   ngOnInit() {}
